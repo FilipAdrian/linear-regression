88d36c021b07c792d62c5a4b095410f68125fcd8
FilipAdrian <adryanfylyp@gmail.com>
2021-03-01 01:52:59 +0200

    Check github action

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import joblib
-
+import pprint
 from linear_regression import *
 
 file_name = "resources/price-model.pkl"
@@ -7,6 +7,4 @@ if __name__ == '__main__':
     data_frame = read_data("./resources/apartmentComplexData.txt")
     heatmap(data_frame)
     regression, score = compute_regression(data_frame, 8)
-    print(score)
     joblib.dump(regression, file_name)
-    # print(predict(regression, [-122, 37, 52, 3549, 707, 1551, 714, 3.691200]))
